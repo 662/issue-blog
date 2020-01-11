d6a7fa90cb2b28ffcd591e6d09c5895621fc6f31
Lost Scarecrow <lya121@gmail.com>
2020-01-11 13:18:57 +0800

    feat: 增加备案号和链接

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './index.scss'
 
 const Footer: React.FC = () => (
-  <section className="m-container m-footer">Copyright © 2019 662.</section>
+  <section className="m-container m-footer">
+    Copyright © 2019 662.
+    <a
+      href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ marginLeft: 16 }}>
+      渝ICP备17011776号-1
+    </a>
+  </section>
 )
 
 export default Footer
